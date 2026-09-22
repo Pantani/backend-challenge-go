3580d6fde8e43a75855292ace57b0307d98ca591
Danilo Pantani <danpantani@gmail.com>
2026-09-22 18:58:21 -0300

    Consolidate initial database migration

--- internal/adapter/postgres/migrations/000002_ledger_wallet_match_and_result_currency.down.sql
+++
@@ -1,5 +0,0 @@
-DROP INDEX IF EXISTS outbox_events_partition_unpublished;
-ALTER TABLE wager_transactions DROP CONSTRAINT IF EXISTS wager_transactions_result_currency;
-ALTER TABLE wager_transactions DROP COLUMN IF EXISTS result_currency;
-DROP TRIGGER IF EXISTS ledger_entries_match_wallet ON ledger_entries;
-DROP FUNCTION IF EXISTS ledger_entries_match_wallet();
--- internal/adapter/postgres/migrations/000003_reference_not_empty_and_outbox_dead_letter.down.sql
+++
@@ -1,6 +0,0 @@
-DROP INDEX IF EXISTS outbox_events_partition_unpublished;
-DROP INDEX IF EXISTS outbox_events_unpublished;
-CREATE INDEX outbox_events_unpublished ON outbox_events (next_attempt_at, seq) WHERE published_at IS NULL;
-CREATE INDEX outbox_events_partition_unpublished ON outbox_events (partition_key, seq) WHERE published_at IS NULL;
-ALTER TABLE outbox_events DROP COLUMN IF EXISTS dead_lettered_at;
-ALTER TABLE wager_transactions DROP CONSTRAINT IF EXISTS wager_transactions_reference_not_empty;
